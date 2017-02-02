24ae27a524fc82b126dcabb31b035bded1d0b314
youjing <onizudango@gmail.com>
2017-02-02 15:28:39 +0100

    key length guesser

---
+++ src/KeyLengthGuesser.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class KeyLengthGuesser {
+	public Map<Integer, Double> guess(String text){
+		HashMap<Integer,Double> guesses = new HashMap<Integer, Double>();
+		for(int i = 1; i <= 16; i++){
+			ArrayList<String> everyStr = this.strToEvery(i, text);
+			Double avg = 0.0;
+			for(int es = 0; es < everyStr.size(); es++){
+				int len = everyStr.get(es).length();
+				HashMap<Character, Integer> occurrences = this.occurrences(everyStr.get(es));
+				Double IoC = this.IoC(occurrences, len);
+				avg = avg + IoC;
+			}
+			Double new_ioc = avg/i;
+			guesses.put(i, new_ioc);
+		}
+		
+		Map<Integer,Double> sorted = new TreeMap(new ValueComparator(guesses));
+		sorted.putAll(guesses);
+		
+		return sorted;
+	}
+	
+	public ArrayList<String> strToEvery(int every, String str){
+		ArrayList<String> arr = new ArrayList<String>();
+		
+		for(int j = 0; j < every; j++){
+			String substr = "";
+			for(int i = 0; i < str.length(); i++){
+				substr = substr + (i % every == j ? str.charAt(i) : "" );
+			}
+			arr.add(substr);
+		}
+		return arr;
+	}
+	
+	public HashMap<Character, Integer> occurrences(String text){
+		HashMap<Character, Integer> arr = new HashMap<Character, Integer>();
+		for(int i = 0; i < text.length(); i++){
+			char letter = text.charAt(i);
+			if(arr.containsKey(letter)){
+				arr.put(letter, arr.get(letter)+1);
+			} else {
+				arr.put(letter, 1);
+			}
+		}
+		
+		return arr;
+	}
+	
+	public Double IoC(HashMap<Character, Integer> occurrences, int len){
+		int sum = 0;
+		for(int count : occurrences.values()){
+			sum = sum + (count * (count - 1));
+			
+		}
+		Double ret = (double)sum/(len * (len-1));
+		return ret;
+	}
+}
+
+class ValueComparator implements Comparator {
+	HashMap map;
+	
+	public ValueComparator(HashMap map){
+		this.map = map;
+	}
+	
+	public int compare(Object keyA, Object keyB){
+		Comparable valueA = (Comparable) map.get(keyA);
+		Comparable valueB = (Comparable) map.get(keyB);
+		return valueB.compareTo(valueA);
+	}
+}
